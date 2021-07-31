110d7ac82dffd2f5d1aa4a9507d18c83126e13e8
Not-found-squad <80631396+Not-found-squad@users.noreply.github.com>
2021-07-31 15:36:34 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,204 @@
+const help = (prefix, uptime, boet, jam, tanggal) => { 
+	return `
+
+┏━━━━━━━━━━┓
+┃  𝗡𝗢𝗧 𝗙𝗢𝗨𝗡𝗗   ┃
+┣━━━━━━━━━━┛
+┣[ *${boet}* ]
+┃
+┣⋗  HOURS : *${jam} WIB*
+┣⋗  DATE : *${tanggal}*
+┗━━━━❪回❫━━━━
+
+╔══════════╗
+┇ 𝗚𝗔𝗠𝗘 𝗠𝗘𝗡𝗨   ┇
+╠══════════╝
+┃
+┣➣ *${prefix}jelajah*
+┣➣ *${prefix}nambang*
+┣➣ *${prefix}nebang*
+┣➣ *${prefix}mancing*
+┣➣ *${prefix}lebur*
+┣➣ *${prefix}jualikan*
+┣➣ *${prefix}jualcoal*
+┣➣ *${prefix}jualstone*
+┣➣ *${prefix}jualingot*
+┣➣ *${prefix}jualkayu*
+┣➣ *${prefix}tebakgambar*
+┣➣ *${prefix}tictactoe*
+┣➣ *${prefix}slot*
+┣➣ *${prefix}inventory*
+┣➣ *${prefix}leaderboard*
+┣➣ *${prefix}level*
+┣➣ *${prefix}suit*
+┣➣ *${prefix}judi*
+┗━━━━━━━━━━━
+
+╔════════════╗
+┇ 𝗦𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴 𝗺𝗲𝗻𝘂   ┇
+╠════════════╝
+┃
+┣➣ *${prefix}play* 
+┣➣ *${prefix}play2*
+┣➣ *${prefix}chara*
+┣➣ *${prefix}gimage*
+┣➣ *${prefix}yts*
+┣➣ *${prefix}pinterest*
+┗━━━━━━━━━━━━
+
+╔══════════╗
+┇ 𝗜𝗠𝗔𝗚𝗘 𝗺𝗲𝗻𝘂   ┇
+╠══════════╝
+┃
+┣➣ *${prefix}waifu* 
+┣➣ *${prefix}nekonime*
+┣➣ *${prefix}husbu*
+┣➣ *${prefix}trap*
+┣➣ *${prefix}nsfwloli*
+┣➣ *${prefix}nsfwwaifu*
+┣➣ *${prefix}nsfwneko*
+┗━━━━━━━━━━━━
+
+╔════════════╗
+┇ 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝗺𝗲𝗻𝘂   ┇
+╠════════════╝
+┃
+┣➣ *${prefix}igdl* [url] 
+┣➣ *${prefix}tiktok* [url]
+┣➣ *${prefix}tiktokmusic* [url]
+┣➣ *${prefix}tiktok2* [url]
+┣➣ *${prefix}tiktoknowm* [url] 
+┣➣ *${prefix}ytmp3* [url] 
+┣➣ *${prefix}ytmp4* [url]
+┣➣ *${prefix}mp3* [url] 
+┣➣ *${prefix}mp4* [url] 
+┣➣ *${prefix}fb* [url]
+┗━━━━━━━━━━━
+╔════════════╗
+┇ 𝗦𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴 𝗺𝗲𝗻𝘂   ┇
+╠════════════╝
+┃
+┣➣ *${prefix}play* 
+┣➣ *${prefix}play2*
+┣➣ *${prefix}chara*
+┣➣ *${prefix}gimage*
+┣➣ *${prefix}yts*
+┣➣ *${prefix}pinterest*
+┗━━━━━━━━━━━━
+
+╔═══════════╗
+┇ 𝗚𝗥𝗢𝗨𝗣 𝗠𝗘𝗡𝗨   ┇
+╠═══════════╝
+┃
+┣➣ *${prefix}grup*
+┣➣ *${prefix}welcome*
+┣➣ *${prefix}linkgroup*
+┣➣ *${prefix}listadmins*
+┣➣ *${prefix}setdescgc*
+┣➣ *${prefix}setnamegc*
+┣➣ *${prefix}demote*
+┣➣ *${prefix}promote*
+┣➣ *${prefix}tagall3*
+┣➣ *${prefix}tagall2*
+┣➣ *${prefix}tagall*
+┗━━━━━━━━━━━━
+
+┏━━━━━━━━━━┓
+┃𝙈𝘼𝙆𝙀𝙍 𝙈𝙀𝙉𝙐  ┃
+┣━━━━━━━━━━┛
+┃
+┣➣ *${prefix}sampah*
+┣➣ *${prefix}delete-file*
+┣➣ *${prefix}bobross*
+┣➣ *${prefix}beautiful*
+┣➣ *${prefix}trash*
+┣➣ *${prefix}pki*
+┣➣ *${prefix}wolverine*
+┣➣ *${prefix}amongus*
+┣━━━━━━━━━
+┣➣ *${prefix}cmm*
+┣➣ *${prefix}kanna*
+┣➣ *${prefix}tweettrump*
+┣➣ *${prefix}naruto* 
+┣➣ *${prefix}breakwall*
+┣➣ *${prefix}matrix*
+┣➣ *${prefix}blueneon*
+┣➣ *${prefix}crosslogo*
+┣➣ *${prefix}flowertext*
+┣➣ *${prefix}wolflogo*
+┣➣ *${prefix}dropwater*
+┣➣ *${prefix}greenneon* 
+┣➣ *${prefix}slapimage*
+┣➣ *${prefix}phkomen*
+┣➣ *${prefix}emoji*
+┣➣ *${prefix}silktext*
+┣➣ *${prefix}flametext*
+┣➣ *${prefix}retrotext*
+┣➣ *${prefix}lighttext* 
+┣➣ *${prefix}cslogo*
+┣➣ *${prefix}skytext*
+┣➣ *${prefix}pubglogo*
+┣➣ *${prefix}smoketext*
+┣➣ *${prefix}glowtext*
+┣➣ *${prefix}glitchtext*
+┣➣ *${prefix}textlight*
+┣➣ *${prefix}leavestext* 
+┣➣ *${prefix}bplogo*
+┣➣ *${prefix}phlogo*
+┣➣ *${prefix}text3d*
+┣➣ *${prefix}text3dbox*
+┣➣ *${prefix}splaybutton*
+┣➣ *${prefix}gplaybutton*
+┣➣ *${prefix}epep*
+┣➣ *${prefix}sandwrite* 
+┣➣ *${prefix}firework*
+┣➣ *${prefix}watercolor*
+┣➣ *${prefix}snowwrite*
+┣➣ *${prefix}crismes*
+┣➣ *${prefix}ttp*
+┣➣ *${prefix}attp*
+┣➣ *${prefix}stiker*
+┣➣ *${prefix}toimg*
+┣➣ *${prefix}tomp4
+┗━━━━━━━━━━━
+
+╔══════════╗
+┇ 𝗢𝗪𝗡𝗘𝗥 𝗺𝗲𝗻𝘂 ┇
+╠══════════╝
+┃
+┣➣ *_`* [eval]
+┣➣ *${prefix}stag* 
+┣➣ *${prefix}hidetag*
+┣➣ *${prefix}imgtag*
+┣➣ *${prefix}kontag*
+┣➣ *${prefix}giftlimit*
+┣➣ *${prefix}addprem*
+┣➣ *${prefix}delprem*
+┣➣ *${prefix}shutdown*
+┣➣ *${prefix}event* 
+┣➣ *${prefix}resetlimit*
+┣➣ *${prefix}fakedeface*
+┣➣ *${prefix}setprefix*
+┣➣ *${prefix}clearall*
+┣➣ *${prefix}bc*
+┣➣ *${prefix}opromote*
+┣➣ *${prefix}odemote*
+┣➣ *${prefix}leave*
+┗━━━━━━━━━━━━
+
+
+┌[ *🅃🄷🄰🄽🄺🅂* ]
+│
+├┲➤ *404 SQUAD*
+│┣⋗ Lingz
+│┣⋗ Felix
+│┣⋗ Lenz
+│┗⋗ Arya
+│
+├⋗ *Radya*
+├⋗ *Rifai*
+└───❪㊌❫───
+`
+}
+
+exports.help = help
